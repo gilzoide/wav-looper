ee4b303c81b667ba46f86c1df74c393d90427ae4
gilzoide <gilzoide@gmail.com>
2017-08-19 15:28:58 -0300

    It works!

--- wav_loopy.py
+++ wav_loopy.py
@@ -20,28 +20,74 @@ Wav-loopy, a simple WAV audio file looper: read file -> write file looped X time
 
 The following formats are allowed for X (using input sample size as basis for time):
   Number "1.2": output size is X * input size
-  Time in seconds "20s": output size is exactly X seconds
-  Adition time in seconds "+10s": output size is input size + X seconds
+  Time in seconds "20.0s": output size is exactly X seconds
+  Adition time in seconds "+10.0s": output size is input size + X seconds
 
 
 Usage:
-    wav_loopy [-f FADE] INPUT OUTPUT X
-    wav_loopy (-h | --help | --version)
+    wav-loopy [-f FADE] [-o OUTPUT] INPUT X
+    wav-loopy (-h | --help | --version)
 
 Options:
-    -h, --help                Show this help.
-    --version                 Show program version.
-    -f FADE, --fade-out=FADE  Fade out the last <fade> seconds linearly [default: 0]
+    -h, --help                  Show this help.
+    --version                   Show program version.
+    -o OUTPUT, --output=OUTPUT  Looped WAV file output name. [default: loopyed.wav]
+    -f FADE, --fade-out=FADE    Fade out the last <fade> seconds linearly. [default: 0]
 """
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 from docopt import docopt
 from scipy.io import wavfile
+import numpy as np
+
+import re
+from math import ceil
+
+class WavLooper:
+    '''WAV manipulation controller'''
+    def __init__(self, filename):
+        self.sample_rate, self.wav = wavfile.read(filename)
+
+    def input_size(self):
+        '''Get the input WAV file size in samples'''
+        return self.wav.shape[0]
+
+    def sec2sample(self, s):
+        '''Transforms from time in seconds to number of samples'''
+        return int(self.sample_rate * s)
+
+    def find_output_size(self, X):
+        '''Find the number of samples of the output'''
+        try:
+            m = re.match(r"(\+?)(.+)s$", X)
+            if not m:
+                return int(float(X) * self.input_size())
+            else:
+                seconds = float(m.group(2))
+                total = self.sec2sample(seconds)
+                if m.group(1) == '+':
+                    total += self.input_size()
+                return total
+        except ValueError:
+            raise ValueError("Invalid format for loop factor {!r}".format(X))
+
+    def loop(self, output_filename, X, fade):
+        '''Loop the input file and write it to output'''
+        output_size = self.find_output_size(X)
+        output = np.tile(self.wav, [ceil(output_size / self.input_size()), 1])[:output_size]
+        if fade:
+            fade_num_samples = self.sec2sample(fade)
+            step = 1.0 / fade_num_samples
+            for i in range(fade_num_samples, 0, -1):
+                output[-i] = np.multiply(output[-i], i * step, casting='unsafe')
+        wavfile.write(output_filename, self.sample_rate, output)
 
 
 def main():
-    arguments = docopt(__doc__, version="wav_loopy " + __version__)
+    arguments = docopt(__doc__, version="wav-loopy " + __version__)
+    looper = WavLooper(arguments['INPUT'])
+    looper.loop(arguments['--output'], arguments['X'], float(arguments['--fade-out']))
 
 
 if __name__ == '__main__':
